b5cf3e4c03fde2d94ab7a4fb8eb52eb6b27439db
zdhniv2006 <zdhniv2006@gmail.com>
2026-05-23 09:22:47 +0700

    Update câu 5.py

--- Kiemtra21.5/câu 5.py
+++ Kiemtra21.5/câu 5.py
@@ -1,17 +1,14 @@
-m = int(input("Nhập số nguyên dương m: "))
-n = int(input("Nhập số nguyên dương n: "))
-
-tong_chu_so_n = 0
-temp = n
+a = int(input("Nhập số nguyên dương a: "))
+b = int(input("Nhập số nguyên dương b: "))
+tong_chu_so_a = 0
+temp = a
 while temp > 0:
     tong_chu_so_n += temp % 10
     temp //= 10
-
-print(f"Tổng các chữ số của n = {tong_chu_so_n}")
-
-if tong_chu_so_n == 0:
-    print("Tổng chữ số bằng 0, không thể chia.")
-elif m % tong_chu_so_n == 0:
-    print(f"{m} chia hết cho {tong_chu_so_n}.")
+print(f"Tổng các chữ số của a = {tong_chu_so_a}")
+if tong_chu_so_a == 0:
+    print("Tổng chữ số bằng 0, không thể chia het.")
+elif m % tong_chu_so_a == 0:
+    print(f"{b} chia hết cho {tong_chu_so_a}.")
 else:
-    print(f"{m} không chia hết cho {tong_chu_so_n}.")
+    print(f"{b} không chia hết cho {tong_chu_so_a}.")

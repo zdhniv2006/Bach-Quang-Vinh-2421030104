383a138c3c75103cc85c5c2be944a49fc16aaacb
zdhniv2006 <zdhniv2006@gmail.com>
2026-05-23 09:08:46 +0700

    Update and rename bt2.py to Bai3.py

---
+++ Chuong2/Bai3.py
@@ -0,0 +1,8 @@
+x = float(input("Nhập v = "))
+y = float(input("Nhập b = "))
+if v == b:
+    print("v = b")
+elif v > b:
+    print("v > b")
+else:
+    print("v < b")
--- Chuong2/bt2.py
+++
@@ -1,9 +0,0 @@
-x = float(input("Nhập x = "))
-y = float(input("Nhập y = "))
-
-if x == y:
-    print("x = y")
-elif x > y:
-    print("x > y")
-else:
-    print("x < y")

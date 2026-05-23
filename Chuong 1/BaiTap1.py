d369e7c61a4f0a5e370244b3a701d1fba7f88545
zdhniv2006 <zdhniv2006@gmail.com>
2026-05-23 09:04:00 +0700

    Update BaiTap1.py

--- Chuong 1/BaiTap1.py
+++ Chuong 1/BaiTap1.py
@@ -1,4 +1,4 @@
-#Nhập vào 1 xâu ký tự có độ dài - 14. In ra kí tự thứ 3, thứ 4-7, thứ 11 và kết câu
+#Nhập vào 1 xâu ký tự có độ dài - 14. In ra kí tự thứ 5, thứ 8-12, thứ 10 và kết thúc
 strl = "Bach Quang Vinh"
 print(strl[3])
 print(strl[4:7])

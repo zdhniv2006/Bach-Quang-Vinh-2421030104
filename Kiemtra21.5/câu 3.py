79a5e882e4441cda39188ace3d2177dc23c3534f
zdhniv2006 <zdhniv2006@gmail.com>
2026-05-23 09:23:18 +0700

    Update câu 3.py

--- Kiemtra21.5/câu 3.py
+++ Kiemtra21.5/câu 3.py
@@ -1,11 +1,9 @@
-n = int(input("Nhập số nguyên dương n: "))
-
+a = int(input("Nhập số nguyên dương a: "))
 tich_chu_so = 1
-temp = n
+temp = a
 while temp > 0:
     tich_chu_so *= temp % 10
     temp //= 10
-
 if tich_chu_so % 2 == 0 and tich_chu_so > 20:
     print(f"Tích các chữ số = {tich_chu_so} → Là số chẵn và lớn hơn 20.")
 else:

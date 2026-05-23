5ff6cbfaa3f3893b86dc7569302548dfd55aba48
zdhniv2006 <zdhniv2006@gmail.com>
2026-05-23 09:06:41 +0700

    Update VD8.py

--- Chuong 1/VD8.py
+++ Chuong 1/VD8.py
@@ -1,4 +1,4 @@
-list1 = ['java', 'python', 'c++']
-list2 = ['c++', 'sql']
+list1 = ['python', 'PHP', 'c++']
+list2 = ['java', 'sql']
 list1.extend(list2);
 print("List sau khi được mở rộng thêm là: ", list1)

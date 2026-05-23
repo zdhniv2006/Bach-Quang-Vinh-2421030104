6efeda525253a894bd77e5d4e013a4de5efb7319
zdhniv2006 <zdhniv2006@gmail.com>
2026-05-23 09:21:27 +0700

    Update câu 2.py

--- Kiemtra21.5/câu 2.py
+++ Kiemtra21.5/câu 2.py
@@ -1,11 +1,9 @@
 n = int(input("Nhập số nguyên dương n: "))
-
 tong_chu_so = 0
 temp = n
 while temp > 0:
     tong_chu_so += temp % 10
     temp //= 10
-
 if tong_chu_so % 3 == 0:
     print(f"Tổng các chữ số = {tong_chu_so} → Chia hết cho 3.")
 else:

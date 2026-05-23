2207741525bcd440339a8f8347a8c713b0db26f7
zdhniv2006 <zdhniv2006@gmail.com>
2026-05-23 09:23:38 +0700

    Update câu 1.py

--- Kiemtra21.5/câu 1.py
+++ Kiemtra21.5/câu 1.py
@@ -3,14 +3,12 @@ x = []
 for i in range(n):
     val = float(input(f"Nhập x{i+1}: "))
     x.append(val)
-
 tong = 0
 dem = 0
 for val in x:
     if 0 < val < 1000:
         tong += val
         dem += 1
-
 if dem > 0:
     print(f"Trung bình cộng: {tong / dem:.2f}")
 else:

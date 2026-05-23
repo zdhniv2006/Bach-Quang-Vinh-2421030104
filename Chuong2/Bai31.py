906a6096f68563a33862885f5955dda87f00cf03
zdhniv2006 <zdhniv2006@gmail.com>
2026-05-23 09:09:56 +0700

    Update and rename bt3.py to Bai31.py

---
+++ Chuong2/Bai31.py
@@ -0,0 +1,13 @@
+madantoc = int(input("Nhap ma dan toc:"))
+if madantoc == 1:
+    print("Dan toc:Dao")
+elif madantoc == 2:
+    print("Dan toc:Tay")
+elif madantoc == 3:
+    print("Dan toc:Nung")
+elif madantoc == 4:
+    print("Dan toc:Thai")
+elif madantoc == 5:
+    print("Dan toc:Khome")
+else:
+    print("ma dan toc khong hop le")

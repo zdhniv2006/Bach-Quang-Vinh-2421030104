47c7bc37219a2a471186bf5fc856b3a49080edf0
zdhniv2006 <zdhniv2006@gmail.com>
2026-05-23 09:23:28 +0700

    Update câu 4.py

--- Kiemtra21.5/câu 4.py
+++ Kiemtra21.5/câu 4.py
@@ -1,9 +1,7 @@
 a = int(input("Nhập số nguyên dương a: "))
 b = int(input("Nhập số nguyên dương b: "))
-
 tong = a + b
 print(f"Tổng (a + b) = {tong}")
-
 chu_so_lon_nhat = 0
 temp = tong
 while temp > 0:
@@ -11,5 +9,4 @@ while temp > 0:
     if chu_so > chu_so_lon_nhat:
         chu_so_lon_nhat = chu_so
     temp //= 10
-
 print(f"Chữ số lớn nhất trong tổng: {chu_so_lon_nhat}")
